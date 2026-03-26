d0f96d1d49ab9084203c94ee05c2c4811e6fa466
Gargoil <emmanueledusei.k@gmail.com>
2026-03-26 10:45:10 +0000

    Initialize Spring Boot project with Maven wrapper

---
+++ src/main/java/com/merce/MerceApplication.java
@@ -0,0 +1,13 @@
+package com.merce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merce/MerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
